4c0081f0bfaf7703680eb668a55896a003df05c5
AAmemo-bit <3690273450101@ingenieria.usac.edu.gt>
2024-07-26 00:06:37 -0600

    Update seno.py

--- seno.py
+++ seno.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-eje_x = np.arange(0, 2*np.pi, 0.01)
-eje_y = np.sin(eje_x)
+x = np.arange(0, 2*np.pi, 0.01)
+y = np.sin(x)
 
-plt.title("Tarea 1")
-plt.xlabel("0 a 2*pi")
-plt.ylabel("sin(x)")
-plt.plot(eje_x, eje_y)
+plt.plot(x, y, 'r')
+plt.title("Función Seno")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.grid(True, linestyle='--')
 plt.show()
