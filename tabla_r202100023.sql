6e98f8ea4da54c23cd28c6ec3b757c78252d04e7
AAmemo-bit <3690273450101@ingenieria.usac.edu.gt>
2024-08-09 20:46:43 -0600

    sql códigos

---
+++ tabla_r202100023.sql
@@ -0,0 +1,13 @@
+create table r202100023(
+	nombre varchar(20),
+	carnet int not null
+);
+
+insert into r202100023 values ('Vilma',123456789);
+
+select * from r202100023;
+
+delete from r202100023 where carnet= ;
+
+
+update r202100023 set carnet = 987654321 where carnet=123123;
