6199b3d074619d52426939c268150dd812154de2
AAmemo-bit <3690273450101@ingenieria.usac.edu.gt>
2024-08-25 19:54:15 -0600

    Procesamiento digital de señales octave y python

---
+++ T4 grabador/octave/tarea4.m
@@ -0,0 +1,77 @@
+if(exist('OCTAVE_VERSION', 'builtin')~=0)
+% estamos en octave
+pkg load signal;
+end
+
+%menu principal
+opcion = 0;
+while opcion ~= 5
+  % opcion = input('seleccione una opcion:\n 1. grabar audio\n 2. reproducir audio\n 3. graficar audio\n 4. salir\n ');
+ %menu de opciones
+disp('1. Grabar')
+disp('2. Reproducir')
+disp('3. Graficar')
+disp('4. Graficar densidad')
+disp('5. Salir')
+opcion = input('Ingrese su eleccion: ');
+
+switch opcion
+  case 1
+    %grabacion de audio
+    try
+      duracion = input('Ingrese la duracion de la grabacion en segundos: ');
+      disp('Comenzando la grabacion...')
+      recObj = audiorecorder;
+      recordblocking(recObj, duracion);
+      disp('Grabacion finalizada');
+      data = getaudiodata(recObj);
+      audiowrite('Alan_octave.wav', data, recObj.SampleRate);
+      disp('Archivo de audio grabado correctamente');
+    catch
+      disp('Errorr al grabar audio');
+    end
+   case 2
+     %reprduccion de audio
+     try
+       disp('Reproduciendo...');
+       [data, fs] = audioread('Alan_octave.wav');
+       sound(data, fs);
+     catch
+       disp('Error al reproducir el audio');
+     end
+    case 3
+     %grafico de audio
+     try
+       [data, fs]=audioread('Alan_octave.wav');
+       tiempo = linspace(0, length(data)/fs, length(data));
+       plot(tiempo, data);
+       xlabel('Tiempo (s)');
+       ylabel('Amplitud');
+       title('Audio');
+      catch
+       disp('Error al graficar el audio.');
+     end
+    case 4
+      %graficando espectro de frecuencia
+      try
+        disp('Graficando espectro de frecuencia...');
+        [audio, Fs] = audioread('Alan_octave.wav'); %lee la señal desde al archivo .wav
+        N = length(audio); %numero de muestras de la señal
+        f = linspace(0, Fs/2, N/2+1); %vector de frecuencias
+        ventana = hann(N); %ventana de hann para reducir el efecto de las discontinuidades al calcular la FFT
+        Sxx = pwelch(audio, ventana, 0, N, Fs); %densidad espectral de potencia
+        plot(f, 10*log10(Sxx(1:N/2+1))); %grafica el espectro de frecuencia en dB
+        xlabel('Frecuencia (Hz)');
+        ylabel('Espectro de frecuencia de la señal grabada');
+      catch
+        disp('Error al graficar el audio');
+      end
+     case 5
+      % salir
+      disp('Saliendo del programa...');
+    otherwise
+      disp('Opcion no valida');
+    end
+   end
+
+
